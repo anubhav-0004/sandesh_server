ae4f02304aa43cde87831d2d917219979a9ae5cc
anubhav-0004 <2003anubhav@gmail.com>
2025-03-26 01:43:40 +0530

    cors

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL, ],
+  origin: ["http://localhost:5173", "http://localhost:4173","https://chat-app-frontend-indol-eight.vercel.app", process.env.CLIENT_URL, ],
   credentials: true,
 }))
 
